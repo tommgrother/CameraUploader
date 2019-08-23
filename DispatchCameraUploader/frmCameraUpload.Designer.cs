3be6af72e5585a58605ed725d2c1c656130e7b96
Tom Grother <tom@designandsupply.co.uk>
2019-08-23 12:40:25 +0100

    Add project files.

---
+++ DispatchCameraUploader/frmCameraUpload.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DispatchCameraUploader
+{
+    partial class frmCameraUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCameraUpload));
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(201, 61);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(13, 35);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(263, 20);
+            this.txtSource.TabIndex = 1;
+            this.txtSource.Text = "e:\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source File:";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(0, 100);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(296, 23);
+            this.progressBar2.TabIndex = 3;
+            // 
+            // frmCameraUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 124);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnUpload);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCameraUpload";
+            this.Text = "Camera Upload";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
---
+++ DispatchCameraUploader/frmCameraUpload.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DispatchCameraUploader
+{
+    public partial class frmCameraUpload : Form
+    {
+        public frmCameraUpload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar2.Minimum = 0;
+                progressBar2.Maximum = 100;
+               
+                progressBar2.Value = 0;
+
+                float totalFileCount;
+                float newFileCount;
+                float progressValue;
+                int progressValueInt;
+
+
+
+                string path = txtSource.Text;
+                var files = Directory.EnumerateFiles(path, "*.jpg", SearchOption.AllDirectories)
+                         .Select(fn => new FileInfo(fn));
+                var fileDateGroups = files.GroupBy(fi => fi.LastWriteTime.Date);
+
+                totalFileCount = files.Count();
+
+
+                
+
+                foreach (var dateGroup in fileDateGroups)
+                {
+                    string dir = Path.Combine(@"\\designsvr1\Public\Dispatch pictures", dateGroup.Key.ToString("yyyy-MM-dd"));
+                    //string dir = Path.Combine(@"C:\Users\Aled\Desktop\2", dateGroup.Key.ToString("yyyy-MM-dd"));
+
+                    Directory.CreateDirectory(dir);
+                    foreach (var file in dateGroup)
+                    {
+                        string newPath = Path.Combine(dir, file.Name);
+                        File.Copy(file.FullName, newPath,true);
+                        File.Delete(file.FullName);
+
+                        newFileCount = files.Count();
+
+                        progressValue = (1 - (newFileCount / totalFileCount)) * 100 ;
+                        progressValueInt = (int)Math.Round(progressValue);
+
+                        progressBar2.Value = progressValueInt;
+                        progressBar2.Update();
+
+
+                    }
+                }
+                MessageBox.Show("File Upload Complete!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            }
+
+        
+    }
+    }
+
